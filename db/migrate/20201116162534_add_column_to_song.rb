0411e8143c49fb09ed3d991f9d21139bb980bfdc
Zane <zanebgriffin@gmail.com>
2020-11-16 10:49:24 -0600

    added default to boolean

--- db/migrate/20201116162534_add_column_to_song.rb
+++ db/migrate/20201116162534_add_column_to_song.rb
@@ -1,5 +1,5 @@
 class AddColumnToSong < ActiveRecord::Migration[6.0]
   def change
-    add_column :songs, :is_favorite, :boolean
+    add_column :songs, :is_favorite, :boolean, default: false
   end
 end
